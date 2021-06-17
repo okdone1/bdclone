f6822ac90712453b761d27b6d742839f3aa863a4
Emon tr <86019997+okdone1@users.noreply.github.com>
2021-06-17 16:53:49 +0600

    Create Emon.py
    $ apt update && apt upgrade
    $ pkg install python
    $ pkg install python2
    $ git clone https://github.com/okdone1/bdclone.git
    $ ls
    $ CD bdclone
    $ python2 Emon.py
    
    User name : emon
    Pass : emom

---
+++ Emon.py
@@ -0,0 +1,9 @@
+
+@@ -125,7 +125,7 @@ def tik():
+\033[1;95m«-_-_-_-_-_-_-_-_-\033[1;91mokdone\033[1;95m-_-_-_-_-_-_-_-_-»
+ """
+CorrectUsername = "emon"
+CorrectPassword = "emon"
+
+loop = 'true'
+while (loop == 'true'):
